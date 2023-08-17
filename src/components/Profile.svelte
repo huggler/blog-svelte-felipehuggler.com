ab0ed88d12342f8a3e0701e54c597a9b53f4ae95
Felipe Huggler <felipe@felipehuggler.com>
2023-08-17 14:40:43 -0300

    build

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,2 +1,2 @@
 <h1 class="title text-3xl font-medium md:text-4xl mb-4">Quem é Felipe Huggler?</h1>
-<img src="/images/felipe-huggler.webp" width="200" height="200" class="m-auto rounded-full mb-10" alt="Felipe Huggler Luz" />
+<img src="/images/felipe-huggler.webp" srcset="images/felipe-huggler-small.png 300w, images/felipe-huggler.webp 1080w" sizes="50vw" width="200" height="200" class="m-auto rounded-full mb-10" alt="Felipe Huggler Luz" />
