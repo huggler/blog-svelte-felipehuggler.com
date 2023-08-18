8bca1bb7f4c2c2b7b4d3556b623fa55dcd93cd01
Felipe Huggler <felipe@felipehuggler.com>
2023-08-18 13:28:47 -0300

    build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Profile from "../components/Profile.svelte";
 </script>
 <svelte:head>
-	<title>FelipeHuggler.com</title>
+	<title>Felipe Huggler | WebSite pessoal com informações profissionais</title>
 	<meta name="description" content="FelipeHuggler.com - perfil pesssoal e profissional" />
 </svelte:head>
 
@@ -11,7 +11,7 @@
 		<Profile />
 		<div class="text-sm md:text-base">
 			<p class="mb-10">
-				Este site tem como objetivo contar um pouco sobre meu perfil pessoal e profissional afim de dar mais confiança e credibilidade para as pessoas e empresas que quiserem se conectar comigo.
+				Este website tem como objetivo contar um pouco sobre meu perfil pessoal e informações profissionais a fim de dar mais confiança e credibilidade para as pessoas e empresas que queiram se conectar comigo.
 			</p>
 			<p class="mb-10">
 				Eu não sou ninguém especial e muito menos um influenciar ou ex-BBB.
@@ -30,7 +30,7 @@
 			</p>
 			
 			<p class="mb-10">
-				Este site também tem o intuito de ser um repositório de tudo aquilo que eu estudo ou vejo no dia a dia e acabo esquecendo.... e vira e volta eu tenho que voltar na internet pra pesquisar sobre o determinado assunto :(
+				Este site também tem o intuito de ser um repositório de tudo aquilo que eu estudo ou vejo no dia a dia e acabo esquecendo.... e vira e mexe eu tenho que voltar a pesquisar sobre o determinado assunto :(
 			</p>
 			<p class="mb-10">
 				Esta aplicação está sendo desenvolvida em <strong>svelte/typescript</strong> e usei o 
@@ -41,6 +41,50 @@
 				  </svg>
 				  </a> para iniciar esta aplicação.
 			</p>
+			<p class="mb-10">
+				Atualmente eu estou focado em aprender mais sobre 
+				<a href="https://tailwindcss.com/" target="_blank" class="font-bold underline inline-flex gap-1">
+					<span>tailwindcss</span>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+				  </svg>
+				  </a> e ferramentas que agilizam o desenvolvimento, tais como:
+	
+						<a href="https://nestjs.com" target="_blank" class="font-bold underline inline-flex gap-1">
+							<span>nestjs</span>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+						  </svg>
+						  </a>, 
+						<a href="https://nextjs.org" target="_blank" class="font-bold underline inline-flex gap-1">
+							<span>nextjs</span>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+						  </svg>
+						  </a>, 
+						  <a href="https://vitejs.dev" target="_blank" class="font-bold underline inline-flex gap-1">
+							<span>vitejs</span>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+						  </svg>
+						  </a>, 
+						  <a href="https://ui.shadcn.com/" target="_blank" class="font-bold underline inline-flex gap-1">
+							<span>shadcn/ui</span>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+						  </svg>
+						  </a>, 
+						  <a href="https://stitches.dev/" target="_blank" class="font-bold underline inline-flex gap-1">
+							<span>stitches</span>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+						  </svg>
+						  </a>
+						</p>
+						<p class="mb-10">Eu também tenho conhecimentos de backend e experiência com nodejs, redis, cache, firebase, sendgrid, mongodb e outras bibliotecas no qual eu pude realizar algums integrações interessantes como: leitura de pdf, geração de pdf, geração de imagens, reconhecimento facial, integração com whatsApp, twillio e por ai vai!</p>
+						<p class="mb-10">E uma área que eu também gosto de atuar é na parte de otimização de aplicações, como por exemplo: SEO e Perfomance, usando os critérios do <strong>Web Core Vitals</strong>.</p>
+
+				
 		</div>
 	</div>
 </section>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,27 @@
 <script>
 	import Profile from "../../components/Profile.svelte";
+	let birthDay = 17;
+	let birthMonth = 10;
+	let birthYear = 1983;
+
+	let currentDay = new Date().getDate();
+	let currentMonth = new Date().getMonth() + 1; // Mês começa de 0 (janeiro) a 11 (dezembro)
+	let currentYear = new Date().getFullYear();
+
+	function calculateAge() {
+    let age = currentYear - birthYear;
+
+    if (birthMonth > currentMonth || (birthMonth === currentMonth && birthDay > currentDay)) {
+      age--;
+    }
+
+    return age;
+  }
+
 </script>
 
 <svelte:head>
-	<title>FelipeHuggler.com - Sobre</title>
+	<title>Felipe Huggler - Um pouco sobre meu perfil pessoal e profissional</title>
 	<meta name="description" content="Sobre Felipe Huggler" />
 </svelte:head>
 
@@ -11,7 +29,34 @@
 	<Profile />
 <div class="text-sm md:text-base">
 		<p class="mb-10">
-			você já leu sobre mim, essa é apenas uma rota no svelte!
+			Meu nome é Felipe Huggler e nasci em 17/101/1983 na cidade de Curitiba/PR e atualmente estou com {calculateAge()} anos de idade
 		</p>
+		<p class="mb-10">
+			Atualmente moro em Piraju, cidade que fica no interior de São Paulo há 400km da capital.
+		</p>
+		<p class="mb-10">
+			Eu tenho 3 filhos e um cachorro e moro junto com minha mulher, que é uma pessoa maravilhosa!
+		</p>
+		<p class="mb-10">
+			Quando eu era criança o meu sonho era ser jogador de futebol, e eu realmente jogava muito bem, chegando a ser destaque em várias competições, porém, o destino não queria que eu seguisse essa carreira e com 10 anos de idade eu cai de um touro mecânico numa dessas festas de rodeio e eu tive uma lesão na coluna.
+		</p>
+		<p class="mb-10">
+			Eu fui operado e graças a Deus, tudo ocorreu bem, porém, tive que abortar o sonho de ser jogador de futebol.
+		</p>
+		<p class="mb-10">
+			Desde então eu comecei a me interessar por tecnologia e na programação foi algo que mexeu comigo.
+		</p>
+		<p class="mb-10">Era mais ou menos em 1996, na época do ICQ, AOL, Mirc, Internet discada... eu comecei a comprar revistas nas bancas de programação, aquelas que vinham com CD de demonstração.</p>
+		<p class="mb-10">Na epóca a linguagem que eu inicie foi ASP, HTML e CSS e logo depois eu descobri que o que fazia a magia acontecer nas aplicações era Javascript!</p>
+		<p class="mb-10">Quando eu tinha 18 anos, fui morar no Rio de Janeiro, onde iniciei minha carreira profissional.</p>
+		<p class="mb-10">Trabalhei em uma agência de websites, onde eu era o responsável por todo o desenvovlvimento e lembro-me que na época os sites ainda eram feitos em tabelas, as aplicações em ASP e ACCESS...Meu Deus! rs</p>
+		<p class="mb-10">Aos poucos eu fui migrando e acompanhando a evolução do mercado, comecei a me dedicar mais na parte do front, sobre tudo em Javascript.</p>
+		<p class="mb-10">Dessa agência web, fui contratado para a CMA que é uma das líderes em desenvolvimento de aplicações para o mercado financeiro e depois dela passei pela <strong>XP Investimentos</strong> e <strong>MyCAP</strong></p>
+		<p class="mb-10">Também trabalhei para <strong>B2W</strong> onde atuei como frontend das marcas Nike.com, CasasBahia.com, PontoFrio.com, Americanas.com, Shoptime, Submarino e SouBarato</p>
+		<p class="mb-10">Trabalhei um tempo para o governo na <strong>ANCINE</strong> - Agencia Nacional do Cinema, onde comecei a programar em React.</p>
+		<p class="mb-10">Passei uma fase trabalhando para uma startup chamada <strong>21k</strong> onde fiz o desenvolvimento sistemas de gestão pública, plataformas de redes sociais, aplicativos em React Native, Angular, Sass... e base de dados como Ne4j, Postgres, MySQL</p>
+		<p class="mb-10">Em 2018 comecei a trilhar minha jornada como triatleta e me dedicar também aos esporte e em pararelo tocar a Odete.com.br, que é uma plataforma de profissionais diaristas.</p>
+		<p class="mb-10">Meus útimos trabalhos foram para uma empresa chamada CellereIt (Campinas) e Woba(MG)</p>
+		<p class="mb-10">Atualmente a Odete caminha sozinha e agora estou em busca de novos desafios!</p>
 	</div>
 </div>
